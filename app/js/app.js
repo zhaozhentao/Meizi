2dc77566e942452146a21a2610f9554503892ace
zhaozhentao <514656282@qq.com>
2015-10-05 10:04:03 +0800

    增加base标签

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,26 @@
-var app = angular.module('meizi', ['ngRoute']);
-
-app.config(['$routeProvider', function($routeProvider){
-  $routeProvider
-    .when('/meizis',{
-      templateUrl: 'part/meizi-list.html',
-      controller: 'MeiziListCtrl'
-    })
-    .otherwise({
-      redirectTo:'/meizis'
-    });
-}]);
-
-app.controller('MeiziListCtrl', ['$scope', function($scope){
-  $scope.meizi = "cyndi";
-}]);
-
+angular.module('meizi', ['ngRoute', 'ngAnimate'])
+  .config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+      $routeProvider
+        .when('/Meizis', {
+          templateUrl: 'part/meizi-list.html',
+          controller: 'Meizis',
+          controllerAs: 'meizis'
+        })
+        .otherwise({
+          redirectTo: '/Meizis'
+        })
+      // configure html5 to get links working on jsfiddle
+      $locationProvider.html5Mode(true);
+  }])
+  .controller('MainCtrl', ['$route', '$routeParams', '$location',
+    function($route, $routeParams, $location) {
+      this.$route = $route;
+      this.$location = $location;
+      this.$routeParams = $routeParams;
+  }])
+  .controller('Meizis', ['$scope', '$routeParams', function($scope, $routeParams) {
+    this.name = "Mezis";
+    this.params = $routeParams;
+    $scope.meizi = "cyndi";
+  }]);
